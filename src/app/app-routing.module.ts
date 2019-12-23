5e7f7b27e7d743943b04d87b55ab77c31f0b8222
Doelia <kyxalia@gmail.com>
2019-12-23 17:42:54 +0100

    :tada: Init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SlidesComponent} from "./slides/slides.component";
+import {PoolComponent} from "./pool/pool.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'project/:id/pool', component: PoolComponent },
+  { path: 'project/:id', component: SlidesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlidesComponent } from './slides/slides.component';
+import {HttpClientModule} from "@angular/common/http";
+import { SlideComponent } from './slide/slide.component';
+import { PoolComponent } from './pool/pool.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlidesComponent,
+    SlideComponent,
+    PoolComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
